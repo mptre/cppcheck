e2de428d833cf207c716e45f8334b862a8c8cc93
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2026-04-09 16:53:26 +0200

    Fix bitwiseOnBoolean warnings in library.cpp (#8428)

--- lib/library.cpp
+++ lib/library.cpp
@@ -1569,8 +1569,7 @@ bool Library::isCompliantValidationExpression(const char* p)
             error |= (*(p + 1) == '-');
         }
         else if (*p == ':') {
-            // cppcheck-suppress bitwiseOnBoolean - TODO: fix this
-            error |= range | (*(p + 1) == '.');
+            error |= range || (*(p + 1) == '.');
             range = true;
             has_dot = false;
             has_E = false;
@@ -1584,8 +1583,7 @@ bool Library::isCompliantValidationExpression(const char* p)
             has_dot = false;
             has_E = false;
         } else if (*p == '.') {
-            // cppcheck-suppress bitwiseOnBoolean - TODO: fix this
-            error |= has_dot | (!std::isdigit(*(p + 1)));
+            error |= has_dot || (!std::isdigit(*(p + 1)));
             has_dot = true;
         } else if (*p == 'E' || *p == 'e') {
             error |= has_E;
