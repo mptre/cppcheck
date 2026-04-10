mptre/cppcheck

commit b599bb40488bd274d7ad2351a7f7622b05300ba5

Fix #14635 FN unusedVariable for array of pointers (#8402)

lib/checkunusedvar.cpp
@@ -424,6 +424,9 @@ static const Token* doAssignment(Variables &variables, const Token *tok, bool de
     Variables::VariableUsage *var1 = variables.find(varid1);
 
     if (var1) {
+        if (var1->mType == Variables::pointerArray)
+            variables.use(varid1, tok);
+
         // jump behind '='
         tok = tok->next();
         while (!tok->isAssignmentOp()) {
@@ -567,8 +570,10 @@ static const Token* doAssignment(Variables &variables, const Token *tok, bool de
                 } else if (var1->mType == Variables::standard && addressOf) {
                     variables.alias(varid1, varid2, true);
                 } else {
-                    if ((var2->mType == Variables::pointer || var2->mType == Variables::pointerArray) && tok->strAt(1) == "[")
+                    if (var2->mType == Variables::pointer || var2->mType == Variables::pointerArray) {
+                        variables.alias(varid1, varid2, true);
                         variables.readAliases(varid2, tok);
+                    }
 
                     variables.read(varid2, tok);
                 }
@@ -1393,7 +1398,6 @@ void CheckUnusedVar::checkFunctionVariableUsage()
 
             // skip things that are only partially implemented to prevent false positives
             if (usage.mType == Variables::pointerPointer ||
-                usage.mType == Variables::pointerArray ||
                 usage.mType == Variables::referenceArray)
                 continue;
 

test/testunusedvar.cpp
@@ -2876,25 +2876,25 @@ private:
                               "{\n"
                               "    int * i[2];\n"
                               "}");
-        TODO_ASSERT_EQUALS("[test.cpp:3:9]: (style) Unused variable: i [unusedVariable]\n", "", errout_str());
+        ASSERT_EQUALS("[test.cpp:3:11]: (style) Unused variable: i [unusedVariable]\n", errout_str());
 
         functionVariableUsage("void foo()\n"
                               "{\n"
                               "    const int * i[2];\n"
                               "}");
-        TODO_ASSERT_EQUALS("[test.cpp:3:9]: (style) Unused variable: i [unusedVariable]\n", "", errout_str());
+        ASSERT_EQUALS("[test.cpp:3:17]: (style) Unused variable: i [unusedVariable]\n", errout_str());
 
         functionVariableUsage("void foo()\n"
                               "{\n"
                               "    void * i[2];\n"
                               "}");
-        TODO_ASSERT_EQUALS("[test.cpp:3:9]: (style) Unused variable: i [unusedVariable]\n", "", errout_str());
+        ASSERT_EQUALS("[test.cpp:3:12]: (style) Unused variable: i [unusedVariable]\n", errout_str());
 
         functionVariableUsage("void foo()\n"
                               "{\n"
                               "    const void * i[2];\n"
                               "}");
-        TODO_ASSERT_EQUALS("[test.cpp:3:9]: (style) Unused variable: i [unusedVariable]\n", "", errout_str());
+        ASSERT_EQUALS("[test.cpp:3:18]: (style) Unused variable: i [unusedVariable]\n", errout_str());
 
         functionVariableUsage("void foo()\n"
                               "{\n"

commit baedd07fea6d281cce0b043f55492da3137dae06

Fix #14661 Regression while handling arrays of function pointers
Regression introduced in PR #8402.

lib/checkunusedvar.cpp
@@ -738,7 +738,9 @@ void CheckUnusedVar::checkFunctionVariableUsage_iterateScopes(const Scope* const
             if (type == Variables::none || isPartOfClassStructUnion(i->typeStartToken()))
                 continue;
             const Token* defValTok = i->nameToken()->next();
-            if (Token::Match(i->nameToken()->previous(), "* %var% ) (")) // function pointer. Jump behind parameter list.
+            while (defValTok && defValTok->str() == "[")
+                defValTok = defValTok->link()->next();
+            if (Token::simpleMatch(defValTok, ") ("))
                 defValTok = defValTok->linkAt(1)->next();
             for (; defValTok; defValTok = defValTok->next()) {
                 if (defValTok->str() == "[")

test/testunusedvar.cpp
@@ -7034,6 +7034,13 @@ private:
                               "    funcPtr();\n"
                               "}");
         ASSERT_EQUALS("", errout_str());
+
+        // #14661
+        functionVariableUsage("int main() {\n"
+                              "    void (*const funcPtr[])(void) = {x};\n"
+                              "    funcPtr[0]();\n"
+                              "}");
+        ASSERT_EQUALS("", errout_str());
     }
 
     void localvarAddr() { // #7747